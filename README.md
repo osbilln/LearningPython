9988ee23ce1a5a36c5bff1d92a1c43ae8fdc4456
Bill Nguyen <billn@bu16640.attlocal.net>
2018-10-10 07:19:48 -0700

    new feature commit

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 ## 01-webotron
 Webotron is a script that will sync a local direcotory to an s3 bucket
+
+- List Bucket
+- List Contents of a bucket
+- Create and setup bucket
--- automating-aws-with-python/01-webotron/webotron/webotron.py
+++ automating-aws-with-python/01-webotron/webotron/webotron.py
@@ -1,5 +1,6 @@
 import boto3
 import click
+from botocore.exceptions import ClientError
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
@@ -14,7 +15,7 @@ def list_buckets():
     "List all s3 buckets"
     for bucket in s3.buckets.all():
         print(bucket)
-    	
+
 @cli.command('list-buckets-objects')
 @click.argument('bucket')
 def list_buckets_objects():
@@ -23,6 +24,55 @@ def list_buckets_objects():
         print(obj)
 #    pass
 
+@cli.command('setup-bucket')
+@click.argument('bucket')
+def setup_bucket(bucket):
+    "Create and configure S3 bucket"
+    s3_bucket = None
+    try:
+        s3_bucket = s3.create_bucket( 
+       	   Bucket=bucket, 
+           CreateBucketConfiguration={'LocationConstraint': 'session.region_name'} 
+        ) 
+    except ClientError as e: 
+        if e.response['Error']['Code'] == 'BucketAlreadyOwnedbyYou': 
+           s3_bucket = s3.Bucket(bucket)
+        else:
+           raise e
+#    s3_bucket = s3.create_bucket(
+#        Bucket='bucket',
+#        CreateBucketConfiguration={'LocationConstraint': 'session.region_name'}
+#    )
+    policy = """
+    {
+      "Version":"2012-10-17",
+      "Statement":[
+        {
+          "Sid":"AddCannedAcl",
+          "Effect":"Allow",
+          "Principal": {"AWS": ["arn:aws:iam::111122223333:root","arn:aws:iam::444455556666:root"]},
+          "Action":["s3:PutObject","s3:PutObjectAcl"],
+          "Resource":["arn:aws:s3:::%s/*"],
+          "Condition":{"StringEquals":{"s3:x-amz-acl":["public-read"]}}
+        }
+      ]
+    }
+    """ % s3_bucket.name
+    policy = policy.strip()
+
+    pol = s3_bucket.Policy()
+    pol.put(Policy=policy)
+    ws = s3_bucket.Website()
+    ws.put(WebsiteConfiguration={
+            'ErrorDocument': {
+                'Key': 'error.html'
+            },
+            'IndexDocument': {
+                'Suffix': 'index.html'
+            }
+    })
+    url = "http://%s.s3-website-us-east-2.amazonaws.com" % s3_bucket.name
+    return
 if __name__ == '__main__':
     cli()
 # list_buckets
